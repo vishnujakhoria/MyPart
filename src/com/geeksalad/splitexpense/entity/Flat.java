eb43034fcecf806958b23d204399243a002c75bd
Vishnu Kumar Jakhoria <vishnu.jakhoria@gmail.com>
2015-04-26 19:14:52 +0530

    First commit
    Change-Id: I194275b70d36c7cd02cb5451361b7ad2a0adb0df
    Signed-off-by: Vishnu Kumar Jakhoria <vishnu.jakhoria@gmail.com>

---
+++ src/com/geeksalad/splitexpense/entity/Flat.java
@@ -0,0 +1,56 @@
+package com.geeksalad.splitexpense.entity;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+
+@PersistenceCapable(identityType = IdentityType.APPLICATION)
+public class Flat {
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	Long id;
+	
+	@Persistent
+	Long Number;
+	
+	@Persistent
+	Long OwnerName;
+	
+	@Persistent
+	Long FlatType;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getNumber() {
+		return Number;
+	}
+
+	public void setNumber(Long number) {
+		Number = number;
+	}
+
+	public Long getOwnerName() {
+		return OwnerName;
+	}
+
+	public void setOwnerName(Long ownerName) {
+		OwnerName = ownerName;
+	}
+
+	public Long getFlatType() {
+		return FlatType;
+	}
+
+	public void setFlatType(Long flatType) {
+		FlatType = flatType;
+	}
+}
